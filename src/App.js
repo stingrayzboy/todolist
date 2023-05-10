0040ba1014b163a7dcd97161dd3c6660dfe44509
Faraz Noor <faraznoor75@gmail.com>
2023-05-10 11:28:14 +0530

    Layout Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import './Layout/Layout.css'
+import Nav from './Layout/Nav';
+import Footer from './Layout/Footer';
 import 'bootstrap/dist/js/bootstrap.js'
+import Todos from './Components/Todos';
 
 function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <>
+    <Nav></Nav>
+    <main className="flex-shrink-0">
+      <div className="container">
+        <Todos/>
+      </div>
+    </main>
+    <Footer></Footer>
+    </>
   );
 }
 
